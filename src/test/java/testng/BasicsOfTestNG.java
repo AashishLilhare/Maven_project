c533d4f8b94d57e8326d94005fbb230ff6f430d5
AashishLilhare <162442434+AashishLilhare@users.noreply.github.com>
2024-03-08 16:58:11 +0530

    Update BasicsOfTestNG.java

--- src/test/java/testng/BasicsOfTestNG.java
+++ src/test/java/testng/BasicsOfTestNG.java
@@ -12,8 +12,7 @@ public class BasicsOfTestNG {
         driver.navigate().to("https://www.facebook.com/");
         driver.manage().window().maximize();
         driver.close();
-        System.out.println("Hello");
-        System.out.println("Hello2");
+        
     }
     @Test
     public void testCase2(){
